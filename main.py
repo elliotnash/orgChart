ce4a6c8f45642be23a688439b9281a1d667703e8
Elliot Nash <elliotnash@gmail.com>
2022-07-29 09:15:25 -0400

    document

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+#!/usr/env python
+
+# To install dependencies, run `pip install -r requirements.txt`
+# run with `python main.py`
+
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.stats import norm
 import csv
 
+# Parses the behavior file into a dictionary
+# To add more behaviours, just append them to the behaviour file, no modifying of code needed
 with open('behaviours', 'r') as file:
     inp = file.read().split('\n\n')
     behaviours = []
@@ -14,7 +19,11 @@ with open('behaviours', 'r') as file:
         if len(cat) == 2:
             dev = []
             for raw_dev in cat[1].split(","):
-                dev.append(list(map(lambda n: float(n), raw_dev.strip().split('-'))))
+                mod_list = list(map(lambda n: float(n), raw_dev.strip().split('-')))
+                dev.append({
+                    "median": mod_list[0],
+                    "std": mod_list[1]
+                })
             behaviours.append({
                 "name": cat[0],
                 "values": {
@@ -24,6 +33,9 @@ with open('behaviours', 'r') as file:
                 }
             })
 
+# Parses the demographics file into a dictionary
+# Every entry in input.csv must have an associated department,
+# and every department must have an associated demographics entry
 with open('demographics', 'r') as file:
     inp = file.read().split('\n\n')
     demographics = {}
@@ -38,12 +50,20 @@ with open('demographics', 'r') as file:
                 dev.append(raw_dev.strip())
             demographics[dem[0].lower()] = {"ethnicity": float(dev[0]), "gender": float(dev[1])}
 
+# a dictionary of every column name (taken from the header) to every index, allowing indexes to not be hard-coded
+indexMap = {}
+# a dictionary of all the input rows with demographic data generated and emails as the key, allowing
+# a manager's entry to be looked up be email
 rowMap = {}
+# the rows to be written to the output csv file
 outRows = []
 
 
+# Randomly generates demographic data for an entry using the parameters from the demographics file
+# To add more categories, the demographic parser, this function,
+# and the generate_behaviours function will need to be modified
 def generate_demographics(row):
-    dep = demographics[row[4].lower().strip()]
+    dep = demographics[row[indexMap['department_name']].lower().strip()]
     if np.random.random() < dep['gender']:
         row.append('Male')
     else:
@@ -52,12 +72,15 @@ def generate_demographics(row):
         row.append("White")
     else:
         row.append("Ethnic Minority")
+    # generates leadership skills value which is added to the provided median value for a behaviour
+    # (90% of the time a value between 0 and 0.5 is generated, 10% a value between -1.5 and -0.5 is generated)
     if np.random.random() > 0.1:
         row.append(np.random.random()*0.5)
     else:
         row.append(np.random.random()-1.5)
 
 
+# generates a score for a behaviour on a 1-7 scale using a provided median and standard deviation
 def generate_score(median, std):
     while True:
         rand = np.random.normal(loc=np.clip(median, 1, 7), scale=std)
@@ -65,45 +88,62 @@ def generate_score(median, std):
             return int(np.round(rand))
 
 
+# find the leadership modifier that is added to the median of all scores generated
+# this works by looking up a boss's leadership value by their email,
+# then recursively finding their boss's leadership value
+# every level up, the leadership value's effect is reduced by half, so an indirect boss's leadership value
+# will propagate down the company, but at a reduced amount each level removed
+# exit case when a boss either has no boss, or they are their own boss
 def get_modifier(email):
     if email in rowMap:
         row = rowMap[email]
-        if email != row[9]:
-            return row[13] + 0.5*get_modifier(row[9])
+        if email != row[indexMap['manager_email']]:
+            return row[indexMap['leadership_skills']] + 0.5*get_modifier(indexMap['manager_email'])
     return 0
 
 
+# Generates behaviours for all behaviours in the behaviours file
 def generate_behaviours(row):
-    modifier = get_modifier(row[9])
+    modifier = get_modifier(row[indexMap['manager_email']])
+    # add the effective modifier that we calculate to the output
     row.append(modifier)
     for behaviour in behaviours:
         val = behaviour['values']
-        median = val['standard'][0]
+        median = val['standard']['median']
+        # med_dif is the effective modifier to be applied to a behavior based off demographic modifiers
+        # and leadership modifiers
         med_dif = 0
-        if row[11].strip().lower() != "male":
-            med_dif += val['gender'][0]-median
-        if row[12].strip().lower() != "white":
-            med_dif += val['ethnicity'][0]-median
+        if row[indexMap['gender']].strip().lower() != "male":
+            med_dif += val['gender']['median']-median
+        if row[indexMap['ethnicity']].strip().lower() != "white":
+            med_dif += val['ethnicity']['median']-median
         median += med_dif + modifier
-        spread = np.max([val['standard'][1], val['gender'][1], val['ethnicity'][1]])
+        # standard deviation is max of all modifiers, so not compounding
+        spread = np.max([val['standard']['std'], val['gender']['std'], val['ethnicity']['std']])
         row.append(generate_score(median, spread))
 
 
+# opens the input file, generates demographic data, and populates rowMap
 with open('input.csv', 'r', encoding='utf-8-sig') as csv_file:
     csv_reader = csv.reader(csv_file)
     for i, row in enumerate(csv_reader):
         if row and i == 0:
-            row.append("Gender")
-            row.append("Ethnicity")
-            row.append("Leadership Skills")
-            row.append("Leadership Modifier")
+            # first row is header, generate modified header
+            row.append("gender")
+            row.append("ethnicity")
+            row.append("leadership_skills")
+            row.append("leadership_modifier")
             for behaviour in behaviours:
                 row.append(behaviour['name'])
+            # populate indexMap
+            for j, name in enumerate(row):
+                indexMap[name.lower()] = j
             outRows.append(row)
         elif row:
             generate_demographics(row)
-            rowMap[row[0]] = row
+            rowMap[row[indexMap['email']]] = row
 
+# generates scores for each entry
 for row in rowMap.values():
     generate_behaviours(row)
     outRows.append(row)
